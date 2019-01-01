f6db693ead3fef60e682d0208e1374fe092f2fa5
Administer <administer@AdministerdeMacBook-Pro.local>
2019-01-01 19:44:48 +0800

    test_change

--- web/config.py
+++ web/config.py
@@ -14,5 +14,5 @@ DATABASE = 'doubanmovie'
 SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4'.format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
 #设置禁用追踪对对象的修改并发送信号
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-#启动自动提交数据库更改在每个请求
+#yes启动自动提交数据库更改在每个请求
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
