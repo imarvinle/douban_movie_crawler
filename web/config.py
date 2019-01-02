1f803f86e378250fb3b0652d3d5ed6c94f5a5a32
marvinle <1634771973@qq.com>
2019-01-02 15:56:23 +0800

    修改默认配置，删减个人数据库配置

--- web/config.py
+++ web/config.py
@@ -8,9 +8,9 @@ DIALECT = 'mysql'
 DRIVER = 'pymysql'
 USERNAME = 'root'
 PASSWORD = 'password'
-HOST = '39.108.123.85'
+HOST = 'ip'
 PORT = '3306'
-DATABASE = 'doubanmovie'
+DATABASE = 'databasename'
 SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4'.format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
 #设置禁用追踪对对象的修改并发送信号
 SQLALCHEMY_TRACK_MODIFICATIONS = False
