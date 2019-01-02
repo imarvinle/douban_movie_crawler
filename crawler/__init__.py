63f02d0c775b2e4f8fe6e3bee2f8868fad7983d2
marvinle <1634771973@qq.com>
2019-01-02 10:00:22 +0800

    format code

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -13,12 +13,9 @@ import random
 import requests
 
 user_agent = [
-    "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1)\
-                Gecko/20061208 Firefox/2.0.0 Opera 9.50",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) \
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729;\
-        .NET CLR 3.5.30729; InfoPath.3; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729;.NET CLR 3.5.30729; InfoPath.3; rv:11.0) like Gecko",
     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
     "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
     "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
