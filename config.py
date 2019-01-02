d00fd849ec5027f8874abee73cef392009772577
marvinle <1634771973@qq.com>
2019-01-02 15:53:53 +0800

    修改默认配置文件

--- config.py
+++ config.py
@@ -14,15 +14,15 @@ DIALECT = 'mysql'               # 使用的数据库
 DRIVER = 'pymysql'              # 驱动 可选: pymysql、 mysqlconnector
 USERNAME = 'root'
 PASSWORD = 'password'
-HOST = '39.108.123.85'
+HOST = 'ip'
 PORT = '3306'
-DATABASE = 'doubanmovie'             # 数据库名
+DATABASE = 'databasename'             # 数据库名
 DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4'.format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
 
 
 # Configure the thread info
 TagThreadSize = 1
 MovieThreadSize = 2
-ShortCommentSize = 0
-CommentSize = 0
-DataBaseInsertSize = 2
+ShortCommentSize = 2
+CommentSize = 2
+DataBaseInsertSize = 4
